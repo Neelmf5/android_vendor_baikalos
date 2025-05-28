652118477f44c7612d91f3fa350dfaffa352d60d
Neel <96389951+Neelmf5@users.noreply.github.com>
2025-05-28 07:34:49 +0530

    cleanup

--- config/baikalos.mk
+++ config/baikalos.mk
@@ -61,9 +61,9 @@ PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
 # Extra packages
-PRODUCT_PACKAGES += \
-    GameSpace \
-    ParallelSpace
+#PRODUCT_PACKAGES += \
+ #   GameSpace \
+  #  ParallelSpace
 
 PRODUCT_PRODUCT_PROPERTIES += \
     persist.face_unlock_service.enabled=true
